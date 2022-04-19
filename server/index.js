180cdc59a14ea7da2cef83cd5eac29cb6ea1fde4
Azat Seyrek <azatseyrek@hotmail.com>
2022-04-19 22:31:10 +0300

    final

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const {Server} = require("socket.io");
-
 app.use(cors());
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -15,17 +15,22 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(`User connected ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(`user with id: ${socket.id} joined room: ${data}`);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
+    console.log("User Disconnected", socket.id);
   });
 });
 
-const port = 3001;
-server.listen(port, () => console.log(`Server started on port ${port}!`));
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
